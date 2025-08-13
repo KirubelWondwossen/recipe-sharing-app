ee56ad2103d2ebcec4c8a59e0f69613f97c985da
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-08-14 02:32:24 +0300

    Make the hero section responsive

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -19,7 +19,7 @@ const heroCards = [
 function HeroSection() {
   return (
     <section>
-      <div className="container mx-auto p-4 flex justify-between h-screen">
+      <div className="container mx-auto p-4 flex flex-col justify-between md:flex-row">
         <HeroLeft />
         <HeroRight />
       </div>
@@ -29,18 +29,18 @@ function HeroSection() {
 
 function HeroLeft() {
   return (
-    <div className="container mx-auto p-4 flex flex-col items-start">
-      <h1 className="font-bold font-heading text-5xl mb-4 text-left mt-20 leading-tight">
+    <div className="container mx-auto p-4 flex flex-col items-center md:items-start">
+      <h1 className="font-bold font-heading text-3xl sm:text-4xl lg:text-5xl mb-4 text-center md:text-left sm:mt-20 leading-tight">
         Cooking Made Fun and Easy: Unleash Your Inner Chef
       </h1>
 
-      <p className="text-base mb-12 md:text-left md:w-10/12 text-gray-400">
+      <p className="md:text-base text-sm mb-7 sm:mb-12 md:text-left w-10/12 text-gray-400">
         Discover more than
         <span className="text-accent font-body"> 10,000 recipes</span> in your
         hand with the best recipe. Help you to find the easiest way to cook.
       </p>
 
-      <button className="px-7 py-3.5 rounded-2xl bg-accent text-white font-body">
+      <button className="text-sm sm:text:base px-7 py-3.5 rounded-2xl bg-accent text-white font-body">
         Explore recipes
       </button>
     </div>
@@ -52,15 +52,15 @@ function HeroRight() {
     <div className="container mx-auto p-4 relative">
       <img className="mb-7" src="images\hero\hero-img.png" />
       <img
-        className="absolute right-0 top-[65%] w-20 h-20"
+        className="absolute right-0 top-[65%] w-11 sm:w-20 sm:h-20"
         src="images\hero\Broccoli.png"
       />
       <img
-        className="absolute top-[10%] w-12.5 h-12.5"
+        className="absolute top-[10%] w-8 sm:w-12.5 sm:h-12.5"
         src="images\hero\Tomato.png"
       />
       <img
-        className="absolute top-[60%] -left-[15%] w-25 h-25"
+        className="absolute top-[60%] -left-[5%] w-14 sm:w-25 sm:h-25"
         src="images\hero\Garlic.png"
       />
       {heroCards.map((element, i) => (
@@ -73,8 +73,10 @@ function HeroRight() {
 function HeroCards({ data, i }) {
   return (
     <div
-      className={`flex flex-col w-52 rounded-xl shadow-lg p-3 gap-4 absolute bg-white ${
-        i === 1 ? "bottom-[20%] left-[45%] " : "bottom-[10%]"
+      className={`invisible sm:visible flex flex-col w-52 rounded-xl shadow-lg p-3 gap-4 absolute bg-white ${
+        i === 1
+          ? "bottom-[20%] md:left-[55%] sm:left-[50%] "
+          : "md:bottom-[10%] sm:bottom-[15%] sm:left-[10%]"
       }`}
     >
       <div className="bg-[#F1F3F7] p-3 rounded-xl">
@@ -85,7 +87,7 @@ function HeroCards({ data, i }) {
               <StarIcon className="w-4 h-4 text-accent" />
             ))}
         </div>
-        <p className="font-body text-left text-sm min-h-[5.5rem]">
+        <p className="font-body text-left text-xs lg:text-sm min-h-[4rem] lg:min-h-[5.5rem]">
           {data.desc}
         </p>
       </div>
@@ -96,9 +98,11 @@ function HeroCards({ data, i }) {
             alt="person"
             className="w-7 h-7 rounded-full"
           />
-          <p className="font-body text-base font-semibold ">{data.name}</p>
+          <p className="font-body text-sm lg:text-base font-semibold ">
+            {data.name}
+          </p>
         </span>
-        <HeartIcon className="w-6" />
+        <HeartIcon className="md:w-6 w-4" />
       </div>
     </div>
   );

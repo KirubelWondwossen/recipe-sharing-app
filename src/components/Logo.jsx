6a89ab2295785be00c2ee352146787e33871c1e4
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-08-13 03:16:08 +0300

    Finish the logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+function Logo() {
+  return (
+    <div className="flex gap-2 items-center container mx-auto px-4">
+      <img src="../../public/images/hero/logo.png" />
+      <span className="text-2xl font-semibold text-logo md:text-xl sm:text-lg">
+        TasteTrail
+      </span>
+    </div>
+  );
+}
+
+export default Logo;

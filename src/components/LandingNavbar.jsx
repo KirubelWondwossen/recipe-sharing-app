ac420fc098e6af0a5bfd808d891ae9e8901e2980
Kirubel Wondwossen <kirubelwondwossen30@gmail.com>
2025-08-30 13:53:49 +0300

    Make the landing page menu open close for small devices

--- src/components/LandingNavbar.jsx
+++ src/components/LandingNavbar.jsx
@@ -1,22 +1,37 @@
 import Logo from "./Logo";
-import { Bars3Icon, MagnifyingGlassIcon } from "@heroicons/react/16/solid";
+import {
+  Bars3Icon,
+  MagnifyingGlassIcon,
+  XMarkIcon,
+} from "@heroicons/react/16/solid";
 import { BookmarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
+import { useState } from "react";
 
 function LandingNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleOpen() {
+    setIsOpen((isOpen) => !isOpen);
+  }
   return (
-    <nav className="container px-4 mx-auto flex justify-between items-center relative">
+    <nav
+      className={`container px-4 mx-auto flex justify-between items-start md:items-center w-full relative`}
+    >
       <Logo />
-      <NavBtns />
-      <NavIcons />
+      <NavBtns isOpen={isOpen} />
+      <NavIcons isOpen={isOpen} handleOpen={handleOpen} />
     </nav>
   );
 }
 
-function NavBtns() {
+function NavBtns({ isOpen }) {
   return (
-    <ul className="invisible md:visible flex md:justify-between md:gap-7 md:flex-row flex-col right-4 sm:right-0 top-0 gap-2 items-start absolute md:static">
+    <ul
+      className={`${
+        !isOpen && "invisible absolute"
+      } md:visible flex-col flex md:justify-between md:gap-7 md:flex-row right-4 sm:right-0 top-0 gap-2 items-start  md:static`}
+    >
       <li className="nav-btn_hover">
         <ScrollLink to="hero" smooth={true} duration={500} className="nav-btn">
           Home
@@ -61,10 +76,20 @@ function NavBtns() {
   );
 }
 
-function NavIcons() {
+function NavIcons({ isOpen, handleOpen }) {
   return (
-    <div className="flex gap-4 items-center px-4">
-      <Bars3Icon className="h-5 w-5 md:hidden" />
+    <div className="flex items-center px-4">
+      {!isOpen ? (
+        <Bars3Icon
+          className={`h-5 w-5 md:hidden cursor-pointer`}
+          onClick={handleOpen}
+        />
+      ) : (
+        <XMarkIcon
+          onClick={handleOpen}
+          className="h-5 w-5  md:hidden cursor-pointer"
+        />
+      )}
       <Link to="/home">
         <MagnifyingGlassIcon className="h-5 w-5 cursor-pointer hidden md:block" />
       </Link>
